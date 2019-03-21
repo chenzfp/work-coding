e486a7fcd133132d472ebe073af7f5c2c67b2d6f
chenzf <chenzfp@gmail.comm>
2019-03-21 15:50:45 +0800

    init

---
+++ work-common/src/main/java/com/czf/common/StudyTest.java
@@ -0,0 +1,15 @@
+package com.czf.common;
+
+/**
+ * @Author chenzf
+ * @Description //TODO
+ * @Date 2019/3/21 15:40
+ */
+public class StudyTest {
+
+    public static void main(String[] args) {
+
+        System.out.println(7>>1);
+    }
+
+}
--- work-common/src/test/java/common/Test.java
+++ work-common/src/test/java/common/Test.java
@@ -1,5 +1,8 @@
 package common;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
 /**
  * @Author chenzf
  * @Description //TODO
@@ -9,8 +12,19 @@ public class Test {
 
     public static void main(String[] args) {
         int a = 10;
-        long b = a >> 1;
+        double b = a >> 2;
         System.out.println(b);
+        DecimalFormat df = new DecimalFormat("#.00");
+        System.out.println(df.format(b));
+        BigDecimal bigDecimal = new BigDecimal(b);
+        System.out.println(bigDecimal);
+        System.out.println(a/4);
+
+
+
+        int c = 7;
+        System.out.println(7<<1);
+        System.out.println(7>>1);
     }
 
 }
